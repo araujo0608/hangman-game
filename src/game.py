f1ae76f6996f1b6ae8f621935621547fbb299fe0
araujo0608 <68195900+araujo0608@users.noreply.github.com>
2022-11-01 21:29:55 -0300

    add lista de tarefas comentadas

--- src/game.py
+++ src/game.py
@@ -6,3 +6,11 @@ from funcoes import *
 
 for index in range(0, 2):
     main(numero_erros, numero_acertos)
+    
+
+'''
+- Colocar letras acertadas no lugar dos respectivos pontilhados
+- Função que verifica se o usuário ganhou ou perdeu
+- Estatisticas de acertos e erros
+- Colocar coloração
+'''
